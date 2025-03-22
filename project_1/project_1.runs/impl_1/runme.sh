95e7916d5351ece2203df979aed97de9babf8c6d
migrell <parkjiho333@naver.com>
2025-03-22 10:11:23 +0900

    프로젝트 완료 _소문자 테스트

--- project_1/project_1.runs/impl_1/runme.sh
+++ project_1/project_1.runs/impl_1/runme.sh
@@ -24,7 +24,7 @@ else
 fi
 export LD_LIBRARY_PATH
 
-HD_PWD='C:/Users/parkj/Desktop/Verilog_project_test/project_1/project_1.runs/impl_1'
+HD_PWD='C:/Users/parkj/Desktop/Verilog_Project2_UART_FIFO_STOPWATCH_WATCH/project_1/project_1.runs/impl_1'
 cd "$HD_PWD"
 
 HD_LOG=runme.log
@@ -41,7 +41,7 @@ EAStep()
 }
 
 # pre-commands:
-/bin/touch .init_design.begin.rst
+/bin/touch .write_bitstream.begin.rst
 EAStep vivado -log top_stopwatch.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source top_stopwatch.tcl -notrace
 
 
